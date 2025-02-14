afb20e4f89b40e2eb277ee4a3eeacd8a693728b6
kazuya <kazuya.osaka.2@gmail.com>
2025-02-14 20:49:24 +0900

    Implement basic front-end UI: base, home, and article detail templates and views

--- core/views.py
+++ core/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+# core/views.py
+from django.shortcuts import render, get_object_or_404
+from core.models import NewsArticle
 
-# Create your views here.
+def home(request):
+    articles = NewsArticle.objects.order_by('-publish_date')
+    return render(request, 'home.html', {'articles': articles})
+
+def article_detail(request, id):
+    article = get_object_or_404(NewsArticle, id=id)
+    return render(request, 'article_detail.html', {'article': article})
--- newsaggregator/settings.py
+++ newsaggregator/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'newsaggregator.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'newsaggregator' / 'templates'],  # ここを追加
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
